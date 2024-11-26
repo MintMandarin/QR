e50742dd041b4c6b7a5e9ad0d1e1e6781a6112d5
Dharmik Patel <dharmikpatel.pepalla@gmail.com>
2024-11-26 15:02:51 +0530

    gh -ages home page url updated

--- src/routes/RoutesWrapper.tsx
+++ src/routes/RoutesWrapper.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import { NotFoundPage, QRScanner, UploadOrderImage } from "../pages";
+import { QRScanner, UploadOrderImage } from "../pages";
 
 export const RoutesWrapper = () => {
   return (
