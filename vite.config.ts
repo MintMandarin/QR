b3896d0df0712a35d99058196fc613fec0237c1a
Dharmik Patel <dharmikpatel.pepalla@gmail.com>
2024-11-26 14:21:32 +0530

    fallback added

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import {ghPages} from "vite-plugin-gh-pages";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), ghPages()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+      },
+    },
+  },
   base: "/QR/",
 });
